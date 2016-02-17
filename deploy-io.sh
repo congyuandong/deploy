131a20cfbdb5160d79febd580ea23a053e24e188
congyuandong <congyuandong@gmail.com>
2016-02-17 15:18:49 +0800

    提交github io 脚本

---
+++ deploy-io.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WEB_PATH='/home/cyd/www/deploy'
+WEB_USER='cyd'
+WEB_USERGROUP='cyd'
+
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished"
